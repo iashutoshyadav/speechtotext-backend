de0c158f9a10b09be50963925e9edd4821382664
Ashutosh Yadav <ashutosh2213072@akgec.ac.in>
2025-07-27 18:30:41 +0530

    Initial commit for backend

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ const transcriptionRoutes = require('./routes/transcription');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://speech-to-text-v892.vercel.app',
+  methods: ['GET', 'POST'],
+  credentials: true
+  
+}));
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/transcription', transcriptionRoutes);
@@ -20,5 +27,4 @@ mongoose.connect(process.env.MONGO_URI, {
 .catch((err) => console.error(' MongoDB Connection Failed:', err));
 
 app.use('/api/transcription', transcriptionRoutes);
-
 app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
